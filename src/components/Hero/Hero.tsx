5b9d9f58f385460d83f5f1b3ceef8024a121d413
DINESH SUTHAR <dineshsutharr427@gmail.com>
2026-04-03 22:43:55 +0530

    change resume

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
               LinkedIn Profile
             </a>
             <a
-              href="https://ik.imagekit.io/ruzb0cftj/Dinesh%20Suthar%20Original.pdf"
+              href="https://ik.imagekit.io/ruzb0cftj/Dinesh_Suthar_Backend_Original.pdf"
               aria-label="View LinkedIn Profile"
               className="text-neutral bg-secondary cursor-pointer rounded-lg px-[14px] py-[10px] text-sm">
               My Resume
